a43442128364b0547e3263537b399f53f283e675
Kyle Davies <kdavies311@gmail.com>
2019-04-17 19:32:45 -0700

    completed array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+var innerArray = [];
+  var output = [];
+
+  if ( size >= array.length) {
+    return [array];
+  } else {
+  while(array.length > 0) {
+    innerArray = array.slice(0,size);
+    output.push(innerArray);
+    array = array.splice(size);
+    }
+
+    return output;
+    }
+};
+
 
 module.exports = chunk;
