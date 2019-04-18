7737ff2382c1338ee7488fc4e32d8d2ff8399d26
Kyle Davies <kdavies311@gmail.com>
2019-04-18 12:56:00 -0700

    steps completed in linear time.

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,25 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/*
+input: number indicating how many steps there will be.
+output: a log, replicating steps / stairs by incrememnting # by 1 as it gets lower. Must contain original width from first step.
+*/
+
+function steps(n) {
+  var steps = "#";
+  var spaces = "";
+// sets up our spaces string to be the correct length
+  for ( var i = 0; i < n-1; i++) {
+    spaces += " ";
+  }
+ // loop up to n, each time logging an additional # and one less space
+  for ( var j = 0; j < n; j++ ) {
+    console.log(steps + spaces);
+    steps += "#";
+    spaces = spaces.slice(1)
+  }
+
+}
 
 module.exports = steps;
